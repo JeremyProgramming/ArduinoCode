9b1f66c862bdf95f9ecf25c848bcd0645494d83b
Jeremy <113724812+JeremyProgramming@users.noreply.github.com>
2023-10-20 19:31:15 +0200

    Update arduino_code.ino

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------------
 /*
 Program:      Final Code for Arduino
-Author:       JeremyProgramming
+Author:       Jeremiah Kamidi
 Date:         18/10/2023
 Version:      3.0
 
@@ -15,90 +15,99 @@ Usage:
   Change the defines underneath to tweak and change the program:
   - soilWet: should be changed if the moisture sensor for any reason
     does not go off
-  - soilDry: value for when the moisture sensor is in the air
+  - air: value for when the moisture sensor is in the air
 
   - Pins: change the values to the appropriate pins. (Note: the
     moisture sensor analog pin should be in an analog pin and the rest
     should be in digital pins)
 
-  - delays: change this value according to how fast your plant drinks water 
-  - miliMetersIncrement: milimeters to add everytime.
-  - startingMiliMeters: first time milimeter addition
-  - miliMeters: Target milimeters the motor should output
+  - delays: change these value according to how fast your plant drinks water 
+  - miliMetersIncrement: milimeters to add everytime
+
+  - miliMetersToOutput: first time milimeter addition
+  - maxWater: Max water before the loop terminates 
 
   - miliLiterPerSecond: Calibrate the motor speed calculated in mm/second 
 */
 
 // Tipping point to water the plant
-#define soilWet 720   // Define min value to consider the soil to be wet
-#define air 950 // Define when the moisture sensor is in the air
+#define soilWet 720                           // Define min value to consider the soil to be wet
+#define air 950                               // Define when the moisture sensor is in the air
 
 // Pins setup
-#define motorPin 3 // Motor digital Pin
-#define moisturePower 2 // Moisture sensor Digital Pin 
-#define moisturePin A0 // Moisture
+#define motorPin 3                            // Motor digital Pin
+#define moisturePower 2                       // Moisture sensor Digital Pin 
+#define moisturePin A0                        // Moisture
 
 // Delay
-#define delayCycle 86400000 // Delay per cycle, currently 1 day
-#define delayInterval 900000 // Delay between intervals
-#define miliMetersToOutput 50 // Add 50 milimeters to
+#define delayCycle 86400000                   // Delay per cycle, currently 1 day
+#define delayInterval 900000                  // Delay between intervals
+
+// Water Specifications
+#define miliMetersToOutput 50                 // Add 50 milimeters to
+#define maxWater 1000                         // Max water before the loop stops
 
 // Motor Specifications
-#define miliLiterPerSecond 20.15// Motor water speed in milimeters per second
+#define miliLiterPerSecond 20.15              // Motor water speed in milimeters per second
 //---------------------------------------------------------------------------------
 
 void setup() {
-  pinMode(moisturePower, OUTPUT); // Set the moisture pin to output
-  pinMode(motorPin, OUTPUT); // Set the motor pin to output
+	pinMode(moisturePower, OUTPUT);       // Set the moisture pin to output
+  pinMode(motorPin, OUTPUT);                  // Set the motor pin to output
 
-  Serial.begin(9600);
+	Serial.begin(9600);
 }
 
 void loop() {
-  int moisture = readMoistureSensor(); // Read the moisture value
+	int moisture = readMoistureSensor();  // Read the moisture value
+  int totalWater = 0;
 
   // Compare the current moisture to the tipping point and check
-  // if the moisture sensor is not in the air Add additional water 
-  // if the moisture value still is low
-  while(moisture >= soilWet && moisture <= air ) {
-    // Power the motor
-    powerMotor(miliMetersToOutput);
+  // if the moisture sensor is not in the air and if the total amount of water
+  // does not exceed the max water. Add additional water if the moisture value still is low
+  while(moisture >= soilWet && moisture <= air 
+        && totalWater <= maxWater) {
+    totalWater += miliMetersToOutput;         // Update total water
+    powerMotor(miliMetersToOutput);           // Power the motor
 
-    // Delay for this interval 
-    delay(delayInterval);
-    moisture = readMoistureSensor();
+    delay(delayInterval);                     // Delay for this interval
+    moisture = readMoistureSensor();          // Reread the moisture
   }
 
-  // Delay for next read
+  
   Serial.println("Wait for the next cycle");
-	delay(delayCycle);
+	delay(delayCycle);                   // Delay for next read              
 }
 
-//  This function returns the humidity of the soil
+// return the humidity of the soil
 int readMoistureSensor() {
-  digitalWrite(moisturePower, HIGH);	// Turn the sensor ON
-  delay(10);							// Allow power to settle
+	digitalWrite(moisturePower, HIGH);   // Turn the sensor ON
+	delay(10);			     // Let the moisture sensor read the input
 
-  int humidity = analogRead(moisturePin);	// Read the value form the moisture sensor
-  digitalWrite(moisturePower, LOW);		// Turn the sensor OFF
+	int humidity = analogRead(moisturePin);// Read the value form the moisture sensor
+	digitalWrite(moisturePower, LOW);    // Turn the sensor off
 	
   Serial.print("Moisture Value:");
   Serial.println(humidity);
-  return humidity;							// Return analog moisture value
+
+  return humidity;			    // Return analog moisture value
 }
 
+
+// Takes a desired amount of milimeters of water and powers the motor
 void powerMotor(int input_milimeters) {
   Serial.println("Pumping water...");
-  digitalWrite(motorPin,HIGH); // Turn on the motor
+  digitalWrite(motorPin,HIGH);               // Turn on the motor
 
   // Calculate the amount of time the motor should be on
   int miliSeconds = (int) ((input_milimeters / miliLiterPerSecond) * 1000); 
+
   Serial.print("Powering motor and adding: ");
   Serial.print(input_milimeters);
   Serial.println("ml to the plant area.");
   
-  delay(miliSeconds); // Wait for the amount of time
+  delay(miliSeconds);                       // Wait for the amount of time
 
-  digitalWrite(motorPin,LOW); // Turn off the motor
+  digitalWrite(motorPin,LOW);               // Turn off the motor
   Serial.println ("Stopped motor...");
 }
